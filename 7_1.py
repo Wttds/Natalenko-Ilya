a43ce592594008bfd64ac5670df59160a78d2652
Natalenko Ilya <115029692+Wttds@users.noreply.github.com>
2022-10-12 10:37:07 +0300

    Update 7_1.py

--- 7_1.py
+++ 7_1.py
@@ -1,19 +1,20 @@
 def main():
     banks = [("sber", 0, 2), ("ZXC", 1, 4), ("VTB", 2, 5), ("a", 3, 3), ("b", 4, 6)]
-    summary, maximum, route, max_route = 0, 0, [], []
+    summary, maximum, route, max_route, inds, max_inds = 0, 0, [], [], [], []
     for i in range(0, len(banks), 2):
         summary += banks[i][2]
         route.append(banks[i][0])
+        inds.append(banks[i][1])
     if summary > maximum:
-        maximum, max_route = summary, route[:]
-    summary, route = 0, []
+        maximum, max_route, max_inds = summary, route[:], inds
+    summary, route, inds = 0, [], []
     for i in range(1, len(banks), 2):
         summary += banks[i][2]
         route.append(banks[i][0])
+        inds.append(banks[i][1])
     if summary > maximum:
-        maximum, max_route = summary, route[:]
-    print(maximum, max_route)
-
+        maximum, max_route, max_inds = summary, route[:], inds
+    print(maximum, *max_route, *max_inds, sep="\n")
 
 main()
 if __name__ == "__main__":
