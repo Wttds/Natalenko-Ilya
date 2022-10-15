f65cdfaac4fb75e73c359a2d16be486b6a25e633
Natalenko Ilya <115029692+Wttds@users.noreply.github.com>
2022-10-15 15:14:44 +0300

    Update 7_1.py

--- 7_1.py
+++ 7_1.py
@@ -2,30 +2,28 @@ def main():
     banks = []
     num_of_banks = int(input())
     names = input().split()
-    indexes = input().split()
     sums = input().split()
     for i in range(num_of_banks):
-        banks.append((names[i], int(indexes[i]), int(sums[i])))
-        
-    summary, maximum, route, max_route, inds, max_inds = 0, 0, [], [], [], []
-    
-    for i in range(0, len(banks), 2):
-        summary += banks[i][2]
-        route.append(banks[i][0])
-        inds.append(banks[i][1])
-    if summary > maximum:
-        maximum, max_route, max_inds = summary, route[:], inds
-        
-    summary, route, inds = 0, [], []
-    
-    for i in range(1, len(banks), 2):
-        summary += banks[i][2]
-        route.append(banks[i][0])
-        inds.append(banks[i][1])
-        
-    if summary > maximum:
-        maximum, max_route, max_inds = summary, route[:], inds
-    print(maximum, max_route, max_inds, sep="\n")
+        banks.append((names[i], i, int(sums[i])))
+
+    route, inds, summary = [], [], 0
+    for i in range(len(banks) - 1):
+        if i - 1 in inds or i in inds:
+            continue
+        elif banks[i][2] >= banks[i + 1][2]:
+            route.append(banks[i][0])
+            inds.append(banks[i][1])
+            summary += banks[i][2]
+        else:
+            route.append(banks[i + 1][0])
+            inds.append(banks[i + 1][1])
+            summary += banks[i + 1][2]
+    if len(banks) - 1 not in inds and len(banks) - 2 not in inds:
+        route.append(banks[-1][0])
+        inds.append(banks[-1][1])
+        summary += banks[-1][2]
+    print(*route)
+    print(summary)
 
 
 main()
